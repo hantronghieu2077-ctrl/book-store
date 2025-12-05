736da46172b6c58c11658f2c2bd7b617e5dc4d72
Hán Trọng Hiếu <hantronghieu2077@gmail.com>
2025-12-05 21:01:47 +0700

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -7,4 +7,4 @@ const UserSchema = new mongoose.Schema({
   email: { type: String }
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('user', UserSchema);
